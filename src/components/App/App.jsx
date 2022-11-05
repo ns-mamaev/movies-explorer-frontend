e662e82c30776fb8eeaa887006da353d3666cd94
Nikita Mamaev <palmally@yandex.ru>
2022-11-05 12:56:31 +0300

    feat: add Header for Landing page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import Main from '../Main/Main';
 import './App.css';
 
 const App = () => {
   return (
     <div className="page">
+      <Header />
       <Main />
       <Footer />
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,14 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <header>
-
+    <header className='header header_place_langing'>
+      <div className='header__logo'></div>
+      <nav className='navigation'>
+        <ul className='navigation__links'>
+          <li><a className='navigation__link' href='#'>Регистрация</a></li>
+          <li><a className='navigation__link navigation__link_hightlighted' href='#'>Войти</a></li>
+        </ul>
+      </nav>
     </header>
   );
 };
