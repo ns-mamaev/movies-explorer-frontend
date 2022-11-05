fbaf71c3093540ddd22c1941ca3f79874a7b68e1
Nikita Mamaev <palmally@yandex.ru>
2022-11-05 12:32:07 +0300

    fix: add styles for learn more btn in Promo

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -7,7 +7,7 @@ const Promo = () => {
         <h1 className='promo__title'>Учебный проект студента факультета Веб&#8209;разработки.</h1>
         <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и&nbsp;его создателя.</p>
         <div className='promo__logo'></div>
-        <button></button>
+        <button className='nav-tab'>Узнать больше</button>
       </div>
     </section>
   )
