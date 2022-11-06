6744b0dad2f7772459f35e925e753af4165836e8
Nikita Mamaev <palmally@yandex.ru>
2022-11-07 00:03:53 +0300

    feat: search form

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,9 +1,11 @@
 import './Movies.css';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import SearchForm from '../SearchForm/SearchForm';
 
 const Movies = () => {
   return (
     <main className='movies'>
+      <SearchForm />
       <MoviesCardList />
     </main>
   );
---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import './SearchForm.css';
+
+function SearchForm () {
+
+  const [value, setValue] = useState('');
+  const [shortFilmsToggle, setShortFilmsToggle] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setValue('');
+    console.log('submit');
+  };
+
+  const onToggle = () => {
+    setShortFilmsToggle(v => !v)
+  };
+
+  return (
+    <form className='search-form' onSubmit={onSubmit}>
+      <div className='search-form__inner'>
+        <input
+          type='text'
+          name='film-search'
+          placeholder='Фильм'
+          required
+          minLength='3'
+          maxLength='300'
+          className='search-form__input'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button
+          type='submit'
+          className='search-form__button'
+        >
+          Поиск
+        </button>
+      </div>
+      <div className='search-form__toggle'>
+      <label className='search-form__toggle-label' htmlFor='short-films'>
+        <input
+            className='search-form__toggle-checkbox-invisible'
+            type='checkbox'
+            name='short-films'
+            id='short-films'
+            checked={shortFilmsToggle}
+            onChange={onToggle}
+          />
+        <span className={`search-form__toggle-checkbox-visible search-form__toggle-checkbox-visible_state_${shortFilmsToggle}`} />
+        Короткометражки
+      </label>
+      <input type='checkbox' />
+      </div>
+    </form>
+  );
+};
+
+export default SearchForm;
