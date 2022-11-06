068ade9233d2f60e30d82a6e597000d86f8dbdb8
Nikita Mamaev <palmally@yandex.ru>
2022-11-06 21:17:01 +0300

    fix: fix colored header width

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,15 @@ import './Header.css';
 const Header = () => {
   return (
     <header className='header header_place_langing'>
-      <div className='header__logo'></div>
-      <nav className='navigation'>
-        <ul className='navigation__links'>
-          <li><a className='navigation__link' href='#'>Регистрация</a></li>
-          <li><a className='navigation__link navigation__link_hightlighted' href='#'>Войти</a></li>
-        </ul>
-      </nav>
+      <div className='header__inner'>
+        <div className='header__logo'></div>
+        <nav className='navigation'>
+          <ul className='navigation__links'>
+            <li><a className='navigation__link' href='#'>Регистрация</a></li>
+            <li><a className='navigation__link navigation__link_hightlighted' href='#'>Войти</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
