1bacd12c25ac6f5aeb1c4f69009cf3443bd92ff9
Nikita Mamaev <palmally@yandex.ru>
2022-11-13 13:09:58 +0300

    fix: add noreferrer attribute

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -15,7 +15,7 @@ const AboutMe = () => {
               Родился и&nbsp;живу в&nbsp;г.Йошкар-Ола, имею магистерскую степень по&nbsp;экономике и&nbsp;большую часть жизни проработал в&nbsp;сфере экономического анализа и&nbsp;бизнес-планирования. У&nbsp;меня есть прекрасная жена. Люблю путешествия в&nbsp;дикую природу и&nbsp;забеги на&nbsp;длинные дистанции. С&nbsp;конца 2021 года начал серьезно увлекаться программированием. А&nbsp;в&nbsp;2022 году прошел большой курс по&nbsp;веб-разработке с&nbsp;код-ревью и&nbsp;бесконечными проектными работами.
             </p>
           </div>
-            <a className='about-me__link' href='https://github.com/ns-mamaev' target='_blank'>
+            <a className='about-me__link' href='https://github.com/ns-mamaev' target='_blank' rel='noreferrer'>
               Github
             </a>
           <img src={avatar} alt='мой портрет' className='about-me__photo' />
