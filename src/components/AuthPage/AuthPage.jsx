4234703623e5021b19259221b6cebe268cf49b6c
palmally <palmally@yandex.ru>
2022-11-24 19:55:38 +0300

    fix: set logo on AuthPage as Link to Landing

--- src/components/AuthPage/AuthPage.jsx
+++ src/components/AuthPage/AuthPage.jsx
@@ -32,7 +32,7 @@ function AuthPage({
   return (
     <main className='auth-page'>
       <div className='auth-page__inner'>
-        <div className='auth-page__logo' />
+        <Link className='auth-page__logo' to='/' />
         <h1 className='auth-page__heading'>{heading}</h1>
         <form className='auth-page__form' name={type} onSubmit={handleSubmit}>
           <fieldset className='auth-page__form-fields'>
